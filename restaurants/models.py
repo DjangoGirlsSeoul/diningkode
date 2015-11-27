bf2d9ad0bc9b9f1e59d57688f792c13039619c34
Jin Park <jin+github@jinpark.net>
2015-11-27 14:10:10 +0900

    add models and migration file

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    price = models.CharField(max_length=20)
+    rating = models.FloatField()
+    image = models.URLField()
+    reviews = models.ForeignKey('Review')
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User)
+    review_text = models.TextField()
+    rating = models.FloatField()
+    
